b9c6976cb61d805742b414a4711a1f41e0ffc0c1
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-03-21 16:18:21 -0300

    update

---
+++ items.js
@@ -0,0 +1,498 @@
+const items = [
+  [
+    "ÁCIDO MEFENÁMICO",
+    440
+  ],
+  [
+    "ACICLOVIR CREMA",
+    590
+  ],
+  [
+    "ACICLOVIR PASTILLA",
+    1350
+  ],
+  [
+    "AMITRIPTILINA",
+    1790
+  ],
+  [
+    "AMOXICILINA (500mg)",
+    890
+  ],
+  [
+    "AMOXICILINA JARABE (250mg)",
+    390
+  ],
+  [
+    "AMOXICILINA JARABE (500mg)",
+    790
+  ],
+  [
+    "AMLODIPINO 5MG",
+    690
+  ],
+  [
+    "AMPICILINA 500mg",
+    990
+  ],
+  [
+    "ASPIRINA 100 (tira)",
+    715
+  ],
+  [
+    "ASPIRINA 500 (tira)",
+    590
+  ],
+  [
+    "ASPIRINA 500 x20",
+    1200
+  ],
+  [
+    "ASPIRINA FORTE 650 MG",
+    620
+  ],
+  [
+    "ALOPURINOL 100 MG",
+    1100
+  ],
+  [
+    "ALOPURINOL 300MG",
+    1290
+  ],
+  [
+    "ATORVASTATINA 20mg",
+    890
+  ],
+  [
+    "ATENOLOL",
+    350
+  ],
+  [
+    "ATENOLOL 50 MG OPKO",
+    50
+  ],
+  [
+    "AZITROMICINA",
+    1190
+  ],
+  [
+    "BETAMETAZONA",
+    450
+  ],
+  [
+    "BUTON (TIRA)",
+    390
+  ],
+  [
+    "CAFIASPIRINA",
+    890
+  ],
+  [
+    "CARVELIDOL 12.5 MG LAB CHILE",
+    1490
+  ],
+  [
+    "CEFADROXILO 500mg",
+    990
+  ],
+  [
+    "CEFALMIN DE 10",
+    710
+  ],
+  [
+    "CEFALMIN DE 20",
+    1150
+  ],
+  [
+    "CELECOXIB DE 10",
+    990
+  ],
+  [
+    "CELECOXIB DE 30",
+    2390
+  ],
+  [
+    "CICLOBENZAPRINA",
+    490
+  ],
+  [
+    "CIPROFLOXACINO",
+    390
+  ],
+  [
+    "CLOBETAZOL CREMA",
+    890
+  ],
+  [
+    "CLONIXINATO DE LISINA",
+    690
+  ],
+  [
+    "CLONIXINATO DE LISINA (COLMAX)",
+    820
+  ],
+  [
+    "CLORFENAMINA",
+    200
+  ],
+  [
+    "CLOTRIMAZOL + BETAMETAZONA",
+    540
+  ],
+  [
+    "CLOTRIMAZOL",
+    350
+  ],
+  [
+    "CLOXACILINA",
+    790
+  ],
+  [
+    "DESLORATADINA",
+    1290
+  ],
+  [
+    "DICLOFENACO (100mg)",
+    450
+  ],
+  [
+    "DICLOFENACO (50mg)",
+    190
+  ],
+  [
+    "DICLOFENACO DIETILAMINA",
+    590
+  ],
+  [
+    "DIPIRONA ( Tira )",
+    135
+  ],
+  [
+    "DISFRUTA CLÁSICO (sobre)",
+    70
+  ],
+  [
+    "DISFRUTA LIMÓN (sobre)",
+    70
+  ],
+  [
+    "DOMPERIDONA",
+    540
+  ],
+  [
+    "ENALAPRIL ",
+    230
+  ],
+  [
+    "ESANDIL",
+    1490
+  ],
+  [
+    "ESOMEPRAZOL 40mg",
+    2990
+  ],
+  [
+    "ESOMEPRAZOL 20mg",
+    2990
+  ],
+  [
+    "LIRACOL ( Nifuroxazida + ftalilsulfatiazol )",
+    2590
+  ],
+  [
+    "FAMOTIDINA",
+    270
+  ],
+  [
+    "FLUCONAZOL",
+    1350
+  ],
+  [
+    "FLUOXETINA",
+    390
+  ],
+  [
+    "FUROSEMIDA",
+    350
+  ],
+  [
+    "GENIOL DIA (sobre)",
+    200
+  ],
+  [
+    "GENIOL NOCHE (sobre)",
+    200
+  ],
+  [
+    "HASSPIRINA 100 MG",
+    229
+  ],
+  [
+    "HASSPIRINA 500 MG",
+    325
+  ],
+  [
+    "HASPIRINA 500 X 20",
+    310
+  ],
+  [
+    "HIDROCLOROTIAZIDA",
+    590
+  ],
+  [
+    "IBUPROFENO 400 UI",
+    1350
+  ],
+  [
+    "IBUPROFENO (400mg)",
+    420
+  ],
+  [
+    "IBUPROFENO (600mg)",
+    540
+  ],
+  [
+    "JARABE HIEDRIX",
+    950
+  ],
+  [
+    "KETOPROFENO (50mg)",
+    650
+  ],
+  [
+    "KETOPROFENO T.U. (200mg)",
+    690
+  ],
+  [
+    "KETOROLACO",
+    340
+  ],
+  [
+    "KITADOL",
+    690
+  ],
+  [
+    "LANSOPRAZOL",
+    990
+  ],
+  [
+    "LOPERAMIDA",
+    250
+  ],
+  [
+    "LORATADINA",
+    420
+  ],
+  [
+    "LOSARTÁN",
+    690
+  ],
+  [
+    "MELATONINA",
+    1490
+  ],
+  [
+    "MELOXICAM",
+    490
+  ],
+  [
+    "MENTHOLATUM",
+    450
+  ],
+  [
+    "METFORMINA 850mg caja",
+    650
+  ],
+  [
+    "METFORMINA 850mg tira",
+    100
+  ],
+  [
+    "MIGRANOL",
+    740
+  ],
+  [
+    "NAPROXENO",
+    750
+  ],
+  [
+    "NASTIZOL",
+    480
+  ],
+  [
+    "NAXODOL PLUS",
+    1290
+  ],
+  [
+    "NITROFURANTOINA",
+    1100
+  ],
+  [
+    "OBLEA CHINA (tira)",
+    520
+  ],
+  [
+    "OMEPRAZOL 20 mg x 30",
+    795
+  ],
+  [
+    "OMEPRAZOL 20 mg x 60",
+    1290
+  ],
+  [
+    "PARACETAMOL ADULTO (tira)",
+    189
+  ],
+  [
+    "PARACETAMOL INFANTIL (tira)",
+    279
+  ],
+  [
+    "PARACETAMOL ADULTO OPKO (tira)",
+    129
+  ],
+  [
+    "PASTILLA CARBÓN (tira)",
+    329
+  ],
+  [
+    "PHILLIPS",
+    990
+  ],
+  [
+    "PIROXICAM",
+    500
+  ],
+  [
+    "PREDUAL (tira)",
+    400
+  ],
+  [
+    "PREDNISONA 5mg",
+    420
+  ],
+  [
+    "PREDNISONA 20mg",
+    1890
+  ],
+  [
+    "PROPANOLOL 40 MG",
+    0
+  ],
+  [
+    "RANITIDINA",
+    620
+  ],
+  [
+    "PREGABALINA 75 MG",
+    3990
+  ],
+  [
+    "SAL BUTAMOL",
+    1200
+  ],
+  [
+    "SERTRALINA",
+    600
+  ],
+  [
+    "SILDENAFILO EN HORA 50 mg",
+    1490
+  ],
+  [
+    "SILDENAFILO EN HORA 100 mg",
+    1650
+  ],
+  [
+    "SILDENAFILO DISILDEN 50mg",
+    2980
+  ],
+  [
+    "SILDENAFILO DISILDEN 100mg",
+    3980
+  ],
+  [
+    "SILDENAFILO ESANTOP 50mg",
+    1690
+  ],
+  [
+    "TRABLETAS ANTIACIDA PASTEURS",
+    330
+  ],
+  [
+    "TAPSIN DÍA PASTILLA (tira)",
+    260
+  ],
+  [
+    "TAPSIN NOCHE PASTILLA (tira)",
+    220
+  ],
+  [
+    "TAPSIN D/N X 18 CAPSULAS",
+    1490
+  ],
+  [
+    "TAPSIN PERÍODO (tira)",
+    450
+  ],
+  [
+    "TRAMADOL PASTILLA (50mg)",
+    950
+  ],
+  [
+    "TRAMADOL GOTAS CHILE",
+    1490
+  ],
+  [
+    "TRAMADOL GOTAS OPKO",
+    1290
+  ],
+  [
+    "TRAMADOL GOTAS ZALEDOR",
+    890
+  ],
+  [
+    "TRAMAVOLTA",
+    1890
+  ],
+  [
+    "TRIMIBUTINO",
+    590
+  ],
+  [
+    "VISINE",
+    0
+  ],
+  [
+    "VITAMINA C FRASCO",
+    1290
+  ],
+  [
+    "VITAMINA C",
+    61.4
+  ],
+  [
+    "VITAMINA E",
+    1290
+  ],
+  [
+    "VITAMINA D",
+    1990
+  ],
+  [
+    "VITAMINA COMPLEX B",
+    2490
+  ],
+  [
+    "VITAMINA MULTIVITAMINICO",
+    1490
+  ],
+  [
+    "ZOPLICONA",
+    1390
+  ],
+  [
+    "ZONIX",
+    1100
+  ]
+];
